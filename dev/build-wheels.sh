e83cd41d9a551586e77208927bc1035864cc9863
joocer <justin.joyce@joocer.com>
2025-12-09 19:14:31 +0000

    try again

--- dev/build-wheels.sh
+++ dev/build-wheels.sh
@@ -18,7 +18,7 @@ cd io
 PYBIN="/opt/python/cp${PYTHON_VERSION//.}-cp${PYTHON_VERSION//.}/bin"
 
 # Install necessary packages
-"${PYBIN}/python" -m pip install -U setuptools wheel setuptools-rust numpy cython auditwheel draken
+"${PYBIN}/python" -m pip install -U setuptools wheel setuptools-rust numpy cython auditwheel
 
 # Build the wheel
 "${PYBIN}/python" setup.py bdist_wheel
