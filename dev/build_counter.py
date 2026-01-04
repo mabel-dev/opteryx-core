543dddc03b0e2a2c556a07afc5771bb4062af9d6
joocer <justin.joyce@joocer.com>
2026-01-04 20:56:44 +0000

    0.5.13

--- dev/build_counter.py
+++ dev/build_counter.py
@@ -29,7 +29,7 @@ class VersionStatus(Enum):
 
 __major_version__ = 0
 __minor_version__ = 5
-__revision_version__ = 12
+__revision_version__ = 13
 __author__ = "@joocer"
 __status__ = VersionStatus.RELEASE
 
