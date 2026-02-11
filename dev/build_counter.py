0a3050de5ddcc9def91fd835396d6fdeac683611
joocer <justin.joyce@joocer.com>
2026-02-11 22:07:22 +0000

    feat: billing bytes

--- dev/build_counter.py
+++ dev/build_counter.py
@@ -29,7 +29,7 @@ class VersionStatus(Enum):
 
 __major_version__ = 0
 __minor_version__ = 6
-__revision_version__ = 17
+__revision_version__ = 18
 __author__ = "@joocer"
 __status__ = VersionStatus.RELEASE
 
