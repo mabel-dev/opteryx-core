6c84507866c0e8a6b1b3e56c4fd2b21fbc379544
Justin Joyce <justin.joyce@joocer.com>
2026-03-31 08:13:47 +0100

    Bump build revision to 57

--- dev/build_counter.py
+++ dev/build_counter.py
@@ -29,7 +29,7 @@ class VersionStatus(Enum):
 
 __major_version__ = 0
 __minor_version__ = 6
-__revision_version__ = 56
+__revision_version__ = 57
 __author__ = "@joocer"
 __status__ = VersionStatus.RELEASE
 
