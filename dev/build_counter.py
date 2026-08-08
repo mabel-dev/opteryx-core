4456bfa7916e31809574c3741ae62a1d022677f9
Justin Joyce <justin.joyce@joocer.com>
2026-08-08 23:23:35 +0100

    Update build counter revision and record improved TPCH benchmark time

--- dev/build_counter.py
+++ dev/build_counter.py
@@ -30,7 +30,7 @@ class VersionStatus(Enum):
 
 __major_version__ = 0
 __minor_version__ = 9
-__revision_version__ = 60
+__revision_version__ = 61
 __author__ = "@joocer"
 __status__ = VersionStatus.RELEASE
 
