d88a9fce2b8144dee1e0b5143b5d6bda31d0bed1
Justin Joyce <justin.joyce@joocer.com>
2026-08-13 21:47:08 +0100

    Bump dev build counter from 65 to 66

--- dev/build_counter.py
+++ dev/build_counter.py
@@ -30,7 +30,7 @@ class VersionStatus(Enum):
 
 __major_version__ = 0
 __minor_version__ = 9
-__revision_version__ = 65
+__revision_version__ = 66
 __author__ = "@joocer"
 __status__ = VersionStatus.RELEASE
 
