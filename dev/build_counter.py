0824bd262a261d0b075664eecae8e8dfb7fd2705
Justin Joyce <justin.joyce@joocer.com>
2026-09-04 20:33:45 +0100

    Update build_counter.py

--- dev/build_counter.py
+++ dev/build_counter.py
@@ -30,7 +30,7 @@ class VersionStatus(Enum):
 
 __major_version__ = 0
 __minor_version__ = 9
-__revision_version__ = 98
+__revision_version__ = 99
 __author__ = "@joocer"
 __status__ = VersionStatus.RELEASE
 
